cdc0714489c5de80a7690a432d67cae661b664f6
Madhusudhan <madhusudhan1863@gmail.com>
2022-12-29 11:24:40 +0530

    fix: add missing clock list view file in previous commits

---
+++ app/src/main/java/com/shreekaram/timepiece/presentation/clock/ClockListView.kt
@@ -0,0 +1,93 @@
+package com.shreekaram.timepiece.presentation.clock
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.shreekaram.timepiece.LocalClockStateViewModel
+import com.shreekaram.timepiece.LocalUTCTimeViewModel
+import com.shreekaram.timepiece.domain.clock.NativeTimezone
+
+@Composable
+fun ClockListView() {
+	val timezones = LocalClockStateViewModel.current.timezones.observeAsState().value!!.toList()
+	val currentTimezone = LocalClockStateViewModel.current.currentTimezone.observeAsState().value!!
+
+	LazyColumn(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(horizontal = 20.dp)
+			.wrapContentHeight()
+			.fillMaxHeight(),
+		verticalArrangement = Arrangement.spacedBy(24.dp),
+		contentPadding = PaddingValues(bottom = 120.dp),
+		userScrollEnabled = timezones.isNotEmpty(),
+	) {
+		item {
+			ClockCanvas(currentTimezone)
+		}
+		items(timezones.size) { index ->
+			val timezone = timezones[index]
+//			loop and pass selected native time zones
+			LocalTimeItem(timezone.second)
+		}
+	}
+}
+
+
+@Composable
+fun LocalTimeItem(timezone: NativeTimezone){
+	val currentDate = LocalUTCTimeViewModel.current.utcDate.observeAsState().value!!
+		.plusHours(timezone.duration.hour)
+		.plusMinutes(timezone.duration.minutes)
+
+	Row(
+		horizontalArrangement = Arrangement.SpaceBetween,
+		modifier = Modifier.fillMaxWidth(),
+		verticalAlignment = Alignment.Top
+	) {
+		Text(
+			text = timezone.cityName,
+			style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.W300)
+		)
+
+		val time = timeFormatter.format(currentDate).split(" ")
+
+		Column(horizontalAlignment = Alignment.End) {
+			Row(verticalAlignment = Alignment.Bottom) {
+				Text(
+					text = time.first(),
+					style = TextStyle(
+						fontSize = 28.sp,
+						fontWeight = FontWeight.W300
+					),
+					modifier = Modifier.alignByBaseline()
+				)
+				Text(
+					text = time.last().lowercase(),
+					style = TextStyle(
+						fontSize = 16.sp,
+						fontWeight = FontWeight.W300
+					),
+					modifier = Modifier.alignByBaseline()
+				)
+			}
+			Text(
+				text = dateFormatter.format(currentDate),
+				style = TextStyle(
+					fontSize = 12.sp,
+					fontWeight = FontWeight.W300,
+					color = Color.Gray
+				)
+			)
+		}
+	}
+}
